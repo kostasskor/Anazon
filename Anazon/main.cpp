59d0f5d2f1bfc620ebd727d2fd379580021c4715
Konstantinos Skordilis <skordilis.skordilis@icloud.com>
2026-09-17 00:22:52 +0300

    Initial Commit - Add welcome menu

--- Anazon/main.cpp
+++ Anazon/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
+void welcome()
+{
+	int option;
+	cout << "Welcome to Anazon!" << endl;
+	cout << "Pick an option:" << endl;
+	cout << "1. Customer" << endl;
+	cout << "2. Seller/Shop" << endl;
+	cout << "3. Exit" << endl;
+	cin >> option;
+}
+
+
 int main()
 {
-
+	welcome();
+	return 0;
 }
